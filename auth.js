0c7470f0093497ddb029899bb42a2d7e2a495a9d
pantazis vastardis <pantazisvastardis@yahoo.com>
2016-08-25 17:32:56 +0300

    xxx

--- auth.js
+++ auth.js
@@ -1,8 +1,10 @@
+// u8_y5FFWh_M2AvvN3VooEiO8
+// client ID 340921863699-csp6142a8v1fcti86c5g6amv7ge21q43.apps.googleusercontent.com
 // The client ID is obtained from the {{ Google Cloud Console }}
 // at {{ https://cloud.google.com/console }}.
 // If you run this code from a server other than http://localhost,
 // you need to register your own client ID.
-var OAUTH2_CLIENT_ID ="AIzaSyDDczfcz2PVNMNBwwonpRI7t3WthynNrOk";
+var OAUTH2_CLIENT_ID ="340921863699-csp6142a8v1fcti86c5g6amv7ge21q43.apps.googleusercontent.com";
 var OAUTH2_SCOPES = [
   'https://www.googleapis.com/auth/youtube'
 ];
